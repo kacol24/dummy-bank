339a7494a8c7623ecd666ce2065f7f7340429de8
Kevin Chandra <kacol.bot@gmail.com>
2024-11-16 12:40:51 +0700

    console command to force trigger disburse job

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Jobs\InterestDisbursement;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('bank:interest-disburse', function (){
+    dispatch(new InterestDisbursement());
+});
